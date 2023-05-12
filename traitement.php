5c03ee1970a8b6bf7620815c06a32ea5e28821f9
Benjamin Louis <louisbenetval@orange.fr>
2023-05-12 02:27:31 +0200

    MAJ

--- traitement.php
+++ traitement.php
@@ -62,47 +62,15 @@
     
     if(isset($_POST['reset'])){
 
-        $_SESSION['products'][] = 0; // Reset toutes les références du tableau.
-        
+        //  unset($_SESSION['products']); // Reset toutes les références du tableau.
+        //  echo $_SESSION['products'][] = 0;
     }
 
     // Pour envoyer une notification de redirection ajouter ?Message=" . urlencode($Message) 
-      
-    header("Location:index.php?Message=" . urlencode($Message));
+
+    header("Location:recap.php?Message=" . urlencode($Message));
     exit();
     
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-    // foreach ($_POST as $key => $value){ // On parcourt le tableau $_POST qui contiendra les clés qui sont envoyées (dans notre cas le bouton supprimer)
-
-    //     if ($value == "Supprimer"){ // On s'occupe de la valeur, "Supprimer" correspond à la value du bouton tandis que la clé c'est l'index des produits.
-    
-    //         foreach ($_SESSION['products'] as $index => $value){ // On parcourt notre tableau des produits et pour chaque index de produit, on va vérifier si il y a un Isset de l'index en question. Le name des boutons correspond à l'index des produits un peu comme un Id unique.
-    
-    //             if (isset($_POST[$index])){
-            
-    //                 unset($_SESSION['products'][$index]); 
-            
-    //                 $_SESSION['nbProducts'] -= 1; // On retire 1 produit du nbProducts puisqu'on en supprime 1.
-            
-    //                 header("Location:recap.php"); // Redirection à la page récap.
-    //                 exit();
-    //             }
-    //         }
-    //     }
-    // }
-    
-    // if (isset($_POST['reset'])){
-    
-    //     unset($_SESSION['products']); 
-    
-    //     $_SESSION['nbProducts'] = 0;
-
-    //     header("Location:recap.php"); // Redirection à la page récap.
-    //     exit();
-    // }
-
-    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
- 
         
 ?>
